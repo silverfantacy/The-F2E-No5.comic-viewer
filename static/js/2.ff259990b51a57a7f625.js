56ad331adca134ff77628bc2cc038d6fe535ddcd
Kuroki_Tsuki <silverfantacy@gmail.com>
2018-07-09 12:21:49 +0800

    Add files via upload

---
+++ static/js/2.ff259990b51a57a7f625.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{n7TL:function(e,t){},riur:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"comicDetail"},[this._v("\r\n  ComicDetail\r\n")])},staticRenderFns:[]};var a=n("VU/8")({name:"comicDetail"},i,!1,function(e){n("n7TL")},"data-v-748ab3a6",null);t.default=a.exports}});
+//# sourceMappingURL=2.ff259990b51a57a7f625.js.map
